90447b1498e2c870444ad57b052081ec92d6568a
mohamdallam <mohamedahmedallam1@gmail.com>
2022-08-19 22:46:47 +0200

    counter

--- src/components/UI/Counter.jsx
+++ src/components/UI/Counter.jsx
@@ -1,15 +1,34 @@
 import "../../styles/counter.css";
 import React from "react";
 
+const COUNTER__DATA = [
+  {
+    numner: "5k+",
+    text: "Clients",
+  },
+
+  {
+    numner: 400,
+    text: "Running Projects",
+  },
+
+  {
+    numner: 500,
+    text: "Projects Completed",
+  },
+];
+
 const Counter = () => {
   return (
-    <section className="counter">
+    <section className="counter" id="projects">
       <div className="container">
         <div className="conter__wrapper">
-          <div className="counter__item">
-            <h3 className="counter__number">2000</h3>
-            <h4 className="counter__title">Clients</h4>
-          </div>
+          {COUNTER__DATA.map((item, index) => (
+            <div className="counter__item" key={index}>
+              <h3 className="counter__number">{item.numner}</h3>
+              <h4 className="counter__title">{item.text}</h4>
+            </div>
+          ))}
         </div>
       </div>
     </section>
